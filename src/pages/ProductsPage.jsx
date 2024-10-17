2c4beba2240d8847c24a440d8cb5dc9f610e630e
Sofia Morales <sofimoralesroa92@gmail.com>
2024-10-17 14:01:20 +0200

    adminProducts

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React from 'react'
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import service from "../services/config.js";
 
 export default function ProductsPage() {
-  return <div>Hola admin</div>;
+
+    const [ allProducts, setAllProducts ] = useState(null)
+
+    useEffect(() => {
+        service.get(`/products`)
+        .then((response) => {
+          //console.log(response.data)
+          setAllProducts(response.data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    
+      }, [])
+
+
+
+  return (
+    <div>
+      todos los productos
+    </div>
+  )
 }
+
+
