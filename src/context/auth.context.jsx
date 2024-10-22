5881e94a37b566ce23767951dc740ae3ada50955
Sofia Morales <sofimoralesroa92@gmail.com>
2024-10-23 00:50:49 +0200

    loading en auth context

--- src/context/auth.context.jsx
+++ src/context/auth.context.jsx
@@ -1,73 +1,66 @@
-
 import { createContext, useEffect, useState } from "react";
 import service from "../services/config";
+import Loading from "../components/Loading";
 
-const AuthContext = createContext()
-
+const AuthContext = createContext();
 
 function AuthWrapper(props) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loggedUserId, setLoggedUserId] = useState(null);
+  const [isValidatingToken, setIsValidatingToken] = useState(true);
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [loggedUserId, setLoggedUserId] = useState(null)
-  const [isValidatingToken, setIsValidatingToken] = useState(true)
-
-  const [isAdmin, setIsAdmin] = useState(false)
+  const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
-    
-    authenticateUser()
-  }, [])
+    authenticateUser();
+  }, []);
 
   const authenticateUser = async () => {
-    
-    
     try {
+      const response = await service.get("/auth/verify");
 
-      const response = await service.get("/auth/verify")
+      console.log(response);
 
-      console.log(response)
-    
-      setIsLoggedIn(true)
-      setLoggedUserId(response.data._id)
-      setIsValidatingToken(false)
+      setIsLoggedIn(true);
+      setLoggedUserId(response.data._id);
+      setIsValidatingToken(false);
 
       if (response.data.role === "admin") {
-        setIsAdmin(true)
+        setIsAdmin(true);
       } else {
-        setIsAdmin(false)
+        setIsAdmin(false);
       }
     } catch (error) {
-     
-      console.log(error)
-      setIsLoggedIn(false)
-      setLoggedUserId(null)
-      setIsValidatingToken(false)
+      console.log(error);
+      setIsLoggedIn(false);
+      setLoggedUserId(null);
+      setIsValidatingToken(false);
 
-      setIsAdmin(false)
+      setIsAdmin(false);
     }
-
-  }
+  };
 
   const passedContext = {
     isLoggedIn,
     loggedUserId,
     authenticateUser,
-    isAdmin
-  }
-
+    isAdmin,
+  };
   if (isValidatingToken) {
-    return <h3>... validando usuario</h3>
+    return (
+      <div className="bg-white my-auto">
+        <div className="bg-white mx-auto">
+          <Loading isLoading={true}></Loading>
+        </div>
+      </div>
+    );
   }
 
   return (
     <AuthContext.Provider value={passedContext}>
       {props.children}
     </AuthContext.Provider>
-  )
-
+  );
 }
 
-export {
-  AuthContext,
-  AuthWrapper
-}
+export { AuthContext, AuthWrapper };
